6f565527d9db65884a2c26b1921497edb3640530
liyinchen <liyc@liyinchen.com>
2019-03-28 15:12:40 +0800

    添加少量注释，改进代码的可读性。

--- slist.c
+++ slist.c
@@ -8,7 +8,7 @@
 
 #include "slist.h"
 
-void initSList(SList **L){
+void initSList(SList **L){//注意此处是二级指针
     *L = (SList *)malloc(sizeof(SList));
     if(*L == NULL) exit(EXIT_FAILURE);
     
@@ -23,16 +23,14 @@ void destroySList(SList **L){
     if(!isExist(*L)) return;
     free((*L)->elem);
     (*L)->elem = NULL;
+    // free和NULL一定要一起使用，因为free只是代表这块内存可以回收，free以后指针还是原来的，不会变成NULL。
     free((*L));
     (*L) = NULL;
 }
 
 void clearSList(SList *L){
-    int i;
     if(!isExist(L)) return;
-    for (i = 0; i < L->len; i++) {
-        L->elem[i] = 0;
-    }
+    L->len = 0; //逻辑删除
 }
 
 bool isEmpty(SList *L){
@@ -66,7 +64,7 @@ int locateELem(SList *L, ElemType e, bool (* compare)(ElemType a, ElemType b)){
     if(!isExist(L)) return INT_MIN;
     
     for (i = 0; i < L->len; i++) {
-        if((*compare)(e, L->elem[i]) == true) return i;
+        if((*compare)(e, getElem(L, i)) == true) return i;
     }
     
     return INT_MIN;
@@ -78,7 +76,7 @@ ElemType priorElem(SList *L, ElemType cur_e){
     if(!isExist(L)) return INT_MIN;
     
     for (i = 0; i < L->len; i++) {
-        if((*compare)(cur_e, L->elem[i]) == true && i > 0) return L->elem[i - 1];
+        if((*compare)(cur_e, getElem(L, i)) == true && i > 0) return getElem(L, i - 1);
     }
     
     return INT_MIN;
@@ -90,7 +88,7 @@ ElemType nextELem(SList *L, ElemType cur_e){
     if(!isExist(L)) return INT_MIN;
     
     for (i = 0; i < L->len; i++) {
-        if((*compare)(cur_e, L->elem[i]) == true && i + 1 < L->len) return L->elem[i + 1];
+        if((*compare)(cur_e, getElem(L, i)) == true && i + 1 < L->len) return getElem(L, i + 1);
     }
     
     return INT_MIN;
@@ -101,7 +99,7 @@ bool insertSList(SList *L, int index, ElemType e){
     
     if(!isExist(L) && (index > L->len || index < 0)) return false;
     
-    if(L->len > L->SListSize){
+    if(L->len == L->SListSize){
         L = realloc(L, (L->SListSize + SListINCREMENT) * sizeof(ElemType));
         if(L == NULL) exit(EXIT_FAILURE);
         L->SListSize += SListINCREMENT;
@@ -123,7 +121,7 @@ bool deleteSList(SList *L, int index){
     for (i = index; i < L->len; i++) {
         L->elem[i] = L->elem[i + 1];
     }
-    L->len--;
+    L->len -= 1;
     
     return true;
 }
@@ -138,7 +136,7 @@ void traverseSList(SList *L, void (* visit)(ElemType e)){
     if(!isExist(L)) return;
     
     for (i = 0; i < L->len; i++) {
-        (*visit)(L->elem[i]);
+        (*visit)(getElem(L, i));
     }
 }
 
@@ -151,7 +149,7 @@ SList * unionSList(SList *La, SList *Lb){
     for (i = La->len, j = 0; j < Lb->len; j++) {
         e = getElem(Lb, j);
         if (locateELem(La, e, &compare) == INT_MIN) {
-            insertSList(La, i++, Lb->elem[j]);
+            insertSList(La, i++, e);
         }
     }
     
@@ -186,7 +184,5 @@ void mergeSList(SList *La, SList *Lb, SList **Lc){
     while(bLen > j){
         e2 = getElem(Lb, j++);
         insertSList(*Lc, k++, e2);
-        
     }
-    
 }
